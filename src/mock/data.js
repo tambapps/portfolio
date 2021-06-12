f98cc1b911a5e58ef09d83477d6dad7a880c8db7
Tambapps <tambapps@gmail.com>
2021-06-12 15:44:39 +0200

    filled project with my data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,27 +2,28 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Tambapps', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Tambapps portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: 'Hi, my name is',
-  name: 'Your Name',
-  subtitle: `I'm the Unknown Developer.`,
+  title: 'Welcome to the portfolio of',
+  name: 'Tambapps',
+  subtitle: ``,
   cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: 'tambapps.jpg',
   paragraphOne:
-    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.',
+    'My name is Nelson FONKOUA. I am a french developer passionated about Java, Groovy and recently Dart',
   paragraphTwo:
-    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.',
+    'I am more interested in back-end projects, but I also develop android (and sometimes desktop) applications',
   paragraphThree: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+  // TODO remove resume button
   resume: 'https://drive.google.com/uc?export=download&id=1UyBpApZhxcS3sKNtKrmRLU_NqIkGRjTa',
   // use Google Drive to upload your file and replace YOUR FILE ID after id=... or use any CDN you prefer
   // Alternatively, empty the string to disable the button if it's not needed
@@ -32,41 +33,30 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: 'Project Title',
-    info:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.',
+    img: 'fandem.webp',
+    title: 'Fandem',
+    info: 'Fandem is a cross-platform (Desktop, Android) project for sharing files between two devices, as long as they are on the same network (Wi-Fi, mobile hotspost). The transfer is made in Peer to Peer, allowing you to keep your data private, and it is way faster than a Bluetooth transfer.',
     info2: '',
-    url: '#!',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: '/fandem', // TODO make page for fandem
+    repo: 'https://github.com/tambapps/P2P-File-Sharing', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: 'Project Title',
-    info:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.',
+    img: 'groovyshell.webp',
+    title: 'Android Groovy Shell',
+    info: 'This is an adaptation of GroovySh for Android, with some enhancements. This app allows you to write and run Groovy code directly from your smartphone. You can do all kinds of things like managing files, perform image processing, display charts and more... There is also a functionality allowing you to launch Groovy scripts in the background (when the app is closed), even periodically',
     info2: '',
-    url: '#!',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://tambapps.github.io/groovy-shell-user-manual/',
+    // no repo (it is private)
   },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: 'Project Title',
-    info:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.',
-    info2: '',
-    url: '#!',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+  // TODO add a project for all software libraries I've created
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: 'Would you like to work with me? Awesome!',
-  btn: `Let's Talk`,
-  email: 'cobimr@protonmail.com',
+  cta: 'Have a question? send a mail to',
+  btn: `tambapps@gmail.com`,
+  email: 'tambapps@gmail.com',
 };
 
 // FOOTER DATA
@@ -75,27 +65,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: 'https://github.com/cobidev/gatsby-simplefolio',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: 'https://github.com/cobidev/gatsby-simplefolio',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: 'https://github.com/cobidev/gatsby-simplefolio',
+      url: 'https://twitter.com/TambApps',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: 'https://github.com/cobidev/gatsby-simplefolio',
+      url: 'https://github.com/tambapps',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://fr.linkedin.com/in/nelson-fonkoua-a9919313a',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
