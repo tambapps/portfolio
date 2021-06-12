f83516823b2a2bd0cc83e16d15a940949fd4b436
Tambapps <tambapps@gmail.com>
2021-06-12 15:56:29 +0200

    small changes

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,12 +19,11 @@ export const heroData = {
 export const aboutData = {
   img: 'tambapps.jpg',
   paragraphOne:
-    'My name is Nelson FONKOUA. I am a french developer passionated about Java, Groovy and recently Dart',
+    'My name is Nelson FONKOUA. I am a french developer passionated about Java, Groovy and recently Dart.',
   paragraphTwo:
-    'I am more interested in back-end projects, but I also develop android (and sometimes desktop) applications',
-  paragraphThree: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-  // TODO remove resume button
-  resume: 'https://drive.google.com/uc?export=download&id=1UyBpApZhxcS3sKNtKrmRLU_NqIkGRjTa',
+    'I am mostly interested in back-end projects, but I also develop android (and sometimes desktop) applications.',
+  paragraphThree: '',
+  // resume: 'https://drive.google.com/uc?export=download&id=1UyBpApZhxcS3sKNtKrmRLU_NqIkGRjTa',
   // use Google Drive to upload your file and replace YOUR FILE ID after id=... or use any CDN you prefer
   // Alternatively, empty the string to disable the button if it's not needed
 };
@@ -35,17 +34,19 @@ export const projectsData = [
     id: nanoid(),
     img: 'fandem.webp',
     title: 'Fandem',
-    info: 'Fandem is a cross-platform (Desktop, Android) project for sharing files between two devices, as long as they are on the same network (Wi-Fi, mobile hotspost). The transfer is made in Peer to Peer, allowing you to keep your data private, and it is way faster than a Bluetooth transfer.',
-    info2: '',
+    info: 'Fandem is a cross-platform (Desktop, Android) project for sharing files between two devices, as long as they are on the same network (Wi-Fi, mobile hotspost).',
+    info2:
+      "The transfer is made in Peer to Peer, allowing you to keep your data private. You'll also notice that it is way faster than a Bluetooth transfer.",
     url: '/fandem', // TODO make page for fandem
     repo: 'https://github.com/tambapps/P2P-File-Sharing', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'groovyshell.webp',
-    title: 'Android Groovy Shell',
-    info: 'This is an adaptation of GroovySh for Android, with some enhancements. This app allows you to write and run Groovy code directly from your smartphone. You can do all kinds of things like managing files, perform image processing, display charts and more... There is also a functionality allowing you to launch Groovy scripts in the background (when the app is closed), even periodically',
-    info2: '',
+    title: 'Groovy Shell for Android',
+    info: 'This app allows you to write and run Groovy code directly from your smartphone through an interactive shell.',
+    info2:
+      'You can do all kinds of things like managing files, perform image processing, display charts and more... There is also a functionality allowing you to launch Groovy scripts in the background (when the app is closed), even periodically',
     url: 'https://tambapps.github.io/groovy-shell-user-manual/',
     // no repo (it is private)
   },
@@ -54,7 +55,7 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: 'Have a question? send a mail to',
+  cta: 'Have a question? Send a mail to',
   btn: `tambapps@gmail.com`,
   email: 'tambapps@gmail.com',
 };
